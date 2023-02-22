218de8baa6ee3371baae5ef910de1aec897255ca
Azigaming404 <120331083+Azigaming404@users.noreply.github.com>
2023-02-22 13:41:19 +0700

    Create redhawk.sh

---
+++ redhawk.sh
@@ -0,0 +1,17 @@
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+blue='\033[0;34m'
+ungu='\033[0;35m'
+Green="\033[32m"
+Red="\033[31m"
+WhiteB="\e[5;37m"
+BlueCyan="\e[5;36m"
+Green_background="\033[42;37m"
+Red_background="\033[41;37m"
+Suffix="\033[0m"
+
+figlet "welcome to redhawk" | lolcat 
+echo "${yellow} ini adalah tool osint"
+echo "cd RED_HAWK && php rhawk.php"
